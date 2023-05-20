f6174bfc6ac6f2e678aa42f973e69385043acca7
Subhash Barela <barelasubhash16@gmail.com>
2023-05-20 19:49:35 +0530

    A Follow Two Or More B

--- Day8_RegaxProgram/Day8_RegaxProgram/Program.cs
+++ Day8_RegaxProgram/Day8_RegaxProgram/Program.cs
@@ -10,7 +10,19 @@ namespace Day8_RegaxProgram
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("welcome Day8 Practice Problem");
+            Pattern pattern = new Pattern();
+            Console.WriteLine("Enter the string : ");
+            string str = Console.ReadLine();
+            bool isValid = pattern.CharMatch(str);
+            if (isValid)
+            {
+                Console.WriteLine("String is valid...!");
+            }
+            else
+            {
+                Console.WriteLine("String is not valid...!");
+            }
+            Console.ReadLine();
         }
     }
 }
